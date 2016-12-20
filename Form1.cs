38e9a8b5d0e594035447835650caf3af548fbbb8
julio cesar <senhorodrigues@live.com>
2016-12-20 18:30:58 -0200

    Funções nas dll.

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows.Forms;
-using System.Xml;
-using System.Net;
-using System.IO;
+using Functions;
 
 namespace ResourceDownloader
 {
@@ -16,7 +14,7 @@ namespace ResourceDownloader
         private bool _stoploop;
         private void Principal_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -53,81 +51,16 @@ namespace ResourceDownloader
 
             #endregion
             _stoploop = false;
-            XmlDocument xmldocument = new XmlDocument();
-            //Carregar xml templatealllist
-            xmldocument.Load(textBox_template.Text);
+
+            Templatealllist templatealllist = new Templatealllist();
+
+            string loc_Template = textBox_template.Text;
             //Salvar o link da resource
             string reslink = textBox_LinkResource.Text;
 
-            #region TemplateAllList
-            string[] pasta = new string[5]
-            {
-                "/00.png",       //0
-                "/1/icon.png",   //1
-                "/1/0/game.png", //2
-                "/1/0/show.png", //3
-                "/1/1/game.png", //4
-            };
-            string[] data = new string[5];
-            string[] baixar;
-            string[] diretorio;
-            foreach (XmlNode selectNode in xmldocument.SelectNodes("/Result/ItemTemplate/*"))
-            {
-                {
-                    #region Pasta ARM
-                    if (selectNode.Attributes["CategoryID"].Value == "7" || selectNode.Attributes["CategoryID"].Value == "27")
-                    {
-                        baixar = new string[5]
-                        {
-                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/00.png",      //0
-                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/icon.png",  //1
-                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/0/game.png",//2
-                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/0/show.png",//3
-                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/1/game.png",//4
-                        };
+            templatealllist.ARM(loc_Template,reslink);
 
-                        diretorio = new string[4]
-                        {
-                        "image/arm/" + selectNode.Attributes["Pic"].Value,          //0
-                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/1",   //1
-                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/0", //2
-                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/1", //3
-                        };
 
-                        string uri = reslink + "image/arm/" + selectNode.Attributes["Pic"].Value;
-
-                        for (int i = 0; i <= 4; i++)
-                        {
-                            //Uni o link com onde irar ser salvo
-                            data[i] = uri + pasta[i];
-                            try
-                            {
-                                //A array diretório só tem 4 elementos.
-                                if (i != 4)
-                                {
-                                    //Criar a pasta.
-                                    if (!Directory.Exists(diretorio[i]))
-                                        Directory.CreateDirectory(diretorio[i]);
-                                }
-
-                                WebClient client = new WebClient();
-
-                                textBox_Load.Text += Environment.NewLine + data[i];
-                                textBox_Load.SelectionStart = textBox_Load.Text.Length;
-                                textBox_Load.ScrollToCaret();
-
-                                client.DownloadFile(data[i], baixar[i]);
-                            }
-                            catch (Exception ex)
-                            {
-                                textBox_Load.Text += Environment.NewLine + "Erro: " + ex.Message;
-                            }
-                        }
-                    }
-                    #endregion
-                }
-            }
-                #endregion
         }
         private void textBox_Load_TextChanged(object sender, EventArgs e)
         {
--- Functions/Templatealllist.cs
+++ Functions/Templatealllist.cs
@@ -1,12 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
+using System.Net;
+using System.IO;
+using System.ComponentModel;
+using System.Windows.Forms;
 
 namespace Functions
 {
+
     public class Templatealllist
     {
+
+        public void ARM(string loc_Template, string reslink)
+        {
+            XmlDocument xmldocument = new XmlDocument();
+            //Carregar xml templatealllist
+            xmldocument.Load(loc_Template);
+            //Salvar o link da resource
+            string[] pasta = new string[5] {
+                "/00.png", //0
+                "/1/icon.png", //1
+                "/1/0/game.png", //2
+                "/1/0/show.png", //3
+                "/1/1/game.png", //4
+		    };
+            string[] data = new string[5];
+            string[] baixar;
+            string[] diretorio;
+            foreach (XmlNode selectNode in xmldocument.SelectNodes("/Result/ItemTemplate/*"))
+            {
+                if (selectNode.Attributes["CategoryID"].Value == "7" || selectNode.Attributes["CategoryID"].Value == "27")
+                {
+                    baixar = new string[5] {
+                    "image/arm/" + selectNode.Attributes["Pic"].Value + "/00.png", //0
+                    "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/icon.png", //1
+                    "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/0/game.png", //2
+                    "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/0/show.png", //3
+                    "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/1/game.png", //4
+				    };
+                    diretorio = new string[4] {
+                        "image/arm/" + selectNode.Attributes["Pic"].Value, //0
+                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/1", //1
+                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/0", //2
+                        "image/arm/" + selectNode.Attributes["Pic"].Value + "/1/1", //3
+				    };
+
+                    string uri = reslink + "image/arm/" + selectNode.Attributes["Pic"].Value;
+
+                    for (int i = 0; i <= 4; i++)
+                    {
+                        //Uni o link com onde irar ser salvo
+                        data[i] = uri + pasta[i];
+                        try
+                        {
+                            //A array diretório só tem 4 elementos.
+                            if (i != 4)
+                            {
+                                //Criar a pasta.
+                                if (!Directory.Exists(diretorio[i]))
+                                    Directory.CreateDirectory(diretorio[i]);
+                            }
+
+                            WebClient client = new WebClient();
+                            
+                            //.Text += Environment.NewLine + data[i];
+                            //textBox_Load.SelectionStart = textBox_Load.Text.Length;
+                            //textBox_Load.ScrollToCaret();
+
+                            client.DownloadFile(data[i], baixar[i]);
+                        }
+                        catch (Exception ex)
+                        {
+                            //textBox_Load.Text += Environment.NewLine + "Erro: " + ex.Message;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
