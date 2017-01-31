3ea20ebff8feee3dc394e1ae56276534c29219a1
Guilherme Henrique <guilhermeskora1@gmail.com>
2017-01-30 23:47:43 -0200

    Fixed

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace ResourceDownloader
         [STAThread]
         static void Main()
         {
+            /*test */
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Principal());
